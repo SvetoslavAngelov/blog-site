8f634b7fe74cfa756fe621df4b6f8ba3797ed806
Svetoslav Angelov <svetoslavangelov86@gmail.com>
2022-11-29 22:00:16 +0000

    Added a button component

--- src/components/buttons/LinkButton.js
+++ src/components/buttons/LinkButton.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import { BodyMain } from "../styles/TextStyles"
+import { BodyMain, SmallText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import { Link } from "gatsby"
 
@@ -8,8 +8,14 @@ export default function LinkButton(props) {
     return (
         <Link to="/page-2">
             <Wrapper>
-                <Title>{props.Title || "Default"}</Title>
-                <Subtitle>{props.Subtitle || "Default"}</ Subtitle>
+                <IconWrapper>
+                    <Icon src="src/images/icons/credit.svg" />
+                    <Ring src="/src/images/icons/icon-ring.svg"/>
+                </IconWrapper>
+                <TextWrapper>
+                    <Title>{props.Title || "Default"}</Title>
+                    <Subtitle>{props.Subtitle || "Default"}</ Subtitle>
+                </ TextWrapper>
             </ Wrapper>
         </ Link>
     )
@@ -23,10 +29,40 @@ const Wrapper = styled.div`
     
     background: linear-gradient(180deg, #FFFFFF 0%, #D9DFFF 100%);
     border-radius: 20px;
+
+    display: grid; 
+    grid-template-columns: 50px auto; 
+
+    align-items: center;
+    gap: 20px;
+`
+const TextWrapper = styled.div`
+    display: grid; 
+    gap: 4px;
 `
 const Title = styled(BodyMain)`
     color: ${themes.lightMode.textSecondary};
 `
-const Subtitle = styled(BodyMain)`
+const Subtitle = styled(SmallText)`
     color: ${themes.lightMode.textSecondary}
+`
+const IconWrapper = styled.div`
+    width: 45px;
+    height: 45px; 
+    background: ${themes.lightMode.backgroundSecondary};
+    border-radius: 50%;
+    display: grid; 
+    // justify = horizontal
+    justify-content: center;
+
+    // align = vertical
+    align-content: center;
+`
+
+const Icon = styled.img`
+    width: 30px;
+    height: 30px; 
+`
+const Ring = styled.img`
+
 `
